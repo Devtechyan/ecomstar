64922fe14f0777eb362443baa29e5dd80df27b30
Yan Aung <yan.aung@plus65.com.sg>
2020-06-18 21:53:27 +0800

    Created eCommerence project with react app

---
+++ Documents/react-course/ecomstar/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.componet';
+
+function App() {
+  return (
+    <div>
+      <div> <HomePage /></div>
+    </div>
+  );
+}
+
+export default App;
