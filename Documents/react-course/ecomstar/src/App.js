6bdf7f84a816f19009793967b1dfc0f6bcb524ff
Yan Aung <yan.aung@plus65.com.sg>
2020-06-19 09:36:06 +0800

    Update our menu-items to navigate withRouter

--- Documents/react-course/ecomstar/src/App.js
+++ Documents/react-course/ecomstar/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <div> <HomePage /></div>
+      <div>
+        <switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/hats" component={HatsPage}></Route>
+        </switch>
+      </div>
     </div>
   );
 }
