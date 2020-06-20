b2772b7fb5bb3ac1d4467501111d7f921b1dad3f
Yan Aung <yan.aung@plus65.com.sg>
2020-06-20 16:09:20 +0800

    Adding header components and links to home

--- Documents/react-course/ecomstar/src/App.js
+++ Documents/react-course/ecomstar/src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.components.jsx';
-
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
       <div>
+        <Header></Header>
         <switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage}></Route>
